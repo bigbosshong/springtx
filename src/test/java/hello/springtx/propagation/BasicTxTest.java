9256c4cb02f41f5a31bddcbae537610addd6a5d3
홍성민 <bigbosshong1101@gmail.com>
2025-10-18 13:28:30 +0900

    Add inner rollback test for requires new transaction behavior in BasicTxTest

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -124,4 +124,20 @@ public class BasicTxTest {
         assertThatException().isThrownBy(() -> txManager.commit(outer))
                         .isInstanceOf(UnexpectedRollbackException.class);
     }
+
+    @Test
+    void inner_rollback_requires_new(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(DefaultTransactionAttribute.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
 }
